0ad6c1dc4f0a4ed69797141a8c8317e24e3705ba
Warren Buckley <wbuckley15@hotmail.com>
2025-03-29 06:30:38 -0400

    WIP - add command line parser

--- CompilerDriver/Program.cs
+++ CompilerDriver/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using System.CommandLine;
 
-Console.WriteLine("Hello, World!");
+
+var lex = new Option<bool>
+(
+    "--lex",
+    "Run the lexer but stop before parsing"
+);
+
+var parse = new Option<bool>
+(
+    "--parse",
+    "Run the lexer and parser but stop before assembly generation"
+);
+
+var codegen = new Option<bool>
+(
+    "--codegen",
+    "Run the lexer, parser, and assembly generation but stop before code generation"
+);
+
+var root = new RootCommand("Compiler Driver");
+root.AddOption(lex);
+root.AddOption(parse);
+root.AddOption(codegen);
+
+root.SetHandler(static async (lex, parse, codegen) =>
+{
+    
+     await Task.CompletedTask;
+}, lex, parse, codegen);
+
+await root.InvokeAsync(args);
+
+
+
+
+
+
