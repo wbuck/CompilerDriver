7b4fb6817099c6a5fedfff463c2ec62cf336fae2
Warren Buckley <wbuckley15@hotmail.com>
2025-03-30 11:56:21 -0400

    WIP - add lexer

--- Compiler.Common.Test/TokenTests.cs
+++ Compiler.Common.Test/TokenTests.cs
@@ -1,4 +1,5 @@
-﻿using Compiler.Common.Tokens;
+﻿using Compiler.Common.Stages;
+using Compiler.Common.Tokens;
 
 namespace Compiler.Common.Test;
 
@@ -8,20 +9,34 @@ public class TokenTests
     public void ParseIdentifierToken()
     {
         const string input = """
+            void add(int a, int b)
+            {
+                return a + b;
+            }
             int main(void) 
             {
-                return 2;
+                return add(1, 2);
             }
             """;
         
         List<IToken> tokens = [];
         IdentifierToken.Parse(input, tokens);
         
-        Assert.Equal(4, tokens.Count);
-        Validate(tokens[0], TokenType.Keyword, "int");
-        Validate(tokens[1], TokenType.Identifier, "main");
-        Validate(tokens[2], TokenType.Keyword, "void");
-        Validate(tokens[3], TokenType.Keyword, "return");
+        Assert.Equal(14, tokens.Count);
+        Validate(tokens[0], TokenType.Keyword, "void");
+        Validate(tokens[1], TokenType.Identifier, "add");
+        Validate(tokens[2], TokenType.Keyword, "int");
+        Validate(tokens[3], TokenType.Identifier, "a");
+        Validate(tokens[4], TokenType.Keyword, "int");
+        Validate(tokens[5], TokenType.Identifier, "b");
+        Validate(tokens[6], TokenType.Keyword, "return");
+        Validate(tokens[7], TokenType.Identifier, "a");
+        Validate(tokens[8], TokenType.Identifier, "b");        
+        Validate(tokens[9], TokenType.Keyword, "int");
+        Validate(tokens[10], TokenType.Identifier, "main");
+        Validate(tokens[11], TokenType.Keyword, "void");
+        Validate(tokens[12], TokenType.Keyword, "return");
+        Validate(tokens[13], TokenType.Identifier, "add");
         return;
         
         void Validate(IToken token, TokenType expectedType,  ReadOnlySpan<char> expectedValue)
@@ -189,6 +204,65 @@ public class TokenTests
         Assert.All(tokens, token => Assert.Equal(TokenType.Semicolon, token.Type));
         Assert.Equal(";", GetSection(input, tokens[0]));
         Assert.Equal(";", GetSection(input, tokens[1]));
+    }
+
+    [Fact]
+    public void LexerTest()
+    {
+        const string input = """
+            void add(int a, int b)
+            {
+                return a + b;
+            }
+            int main(void) 
+            {
+               return add(1, 2);
+            }
+            """;
+        
+        var tokens = Lexer.Lex(input);
+        Assert.Equal(30, tokens.Count);
+        Validate(tokens[0], TokenType.Keyword, "void");
+        Validate(tokens[1], TokenType.Identifier, "add");
+        Validate(tokens[2], TokenType.OpenParenthesis, "(");
+        Validate(tokens[3], TokenType.Keyword, "int");
+        Validate(tokens[4], TokenType.Identifier, "a");
+        Validate(tokens[5], TokenType.Comma, ",");               
+        Validate(tokens[6], TokenType.Keyword, "int");
+        Validate(tokens[7], TokenType.Identifier, "b");
+        Validate(tokens[8], TokenType.CloseParenthesis, ")");
+        Validate(tokens[9], TokenType.OpenBrace, "{");
+        Validate(tokens[10], TokenType.Keyword, "return");
+        Validate(tokens[11], TokenType.Identifier, "a");
+        Validate(tokens[12], TokenType.Identifier, "b");
+        Validate(tokens[13], TokenType.Semicolon, ";");
+        Validate(tokens[14], TokenType.CloseBrace, "}");
+        Validate(tokens[15], TokenType.Keyword, "int");
+        Validate(tokens[16], TokenType.Identifier, "main");
+        Validate(tokens[17], TokenType.OpenParenthesis, "(");        
+        Validate(tokens[18], TokenType.Keyword, "void");
+        Validate(tokens[19], TokenType.CloseParenthesis, ")");
+        Validate(tokens[20], TokenType.OpenBrace, "{");
+        Validate(tokens[21], TokenType.Keyword, "return");
+        Validate(tokens[22], TokenType.Identifier, "add");
+        Validate(tokens[23], TokenType.OpenParenthesis, "(");
+        Validate(tokens[24], TokenType.IntegralConstant, "1");
+        Validate(tokens[25], TokenType.Comma, ",");
+        Validate(tokens[26], TokenType.IntegralConstant, "2");
+        Validate(tokens[27], TokenType.CloseParenthesis, ")");
+        Validate(tokens[28], TokenType.Semicolon, ";");
+        Validate(tokens[29], TokenType.CloseBrace, "}");
+        return;
+        
+        void Validate(IToken token, TokenType expectedType,  ReadOnlySpan<char> expectedValue)
+        {
+            Assert.Equal(expectedValue, GetSection(input, token));
+            Assert.Equal(expectedType, token.Type);
+            if (token is IdentifierToken { Type: TokenType.Keyword } keywordToken)
+            {
+                Assert.Equal(expectedValue, keywordToken.Keyword);
+            }
+        }
     }
     
     private static ReadOnlySpan<char> GetSection(string input, IToken token) 
---
+++ Compiler.Common/Stages/Lexer.cs
@@ -0,0 +1,29 @@
+﻿using Compiler.Common.Tokens;
+
+namespace Compiler.Common.Stages;
+
+public static class Lexer
+{
+    public static List<IToken> Lex(ReadOnlySpan<char> fileContent)
+    {
+        List<IToken> tokens = [];
+        
+        Parse<OpenBraceToken>(fileContent, tokens);
+        Parse<CloseBraceToken>(fileContent, tokens);
+        Parse<OpenParenthesisToken>(fileContent, tokens);
+        Parse<CloseParenthesisToken>(fileContent, tokens);
+        Parse<CommaToken>(fileContent, tokens);
+        Parse<IdentifierToken>(fileContent, tokens);
+        Parse<SemiColonToken>(fileContent, tokens);
+        Parse<IntegralConstantToken>(fileContent, tokens);
+        
+        tokens.Sort((t1, t2) => t1.Index.CompareTo(t2.Index));
+        
+        return tokens;
+        
+        static void Parse<T>(ReadOnlySpan<char> fileContent, in List<IToken> tokens) where T : IToken
+            => T.Parse(fileContent, tokens);
+    }
+
+    
+}
