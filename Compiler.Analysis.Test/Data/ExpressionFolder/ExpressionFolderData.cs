141d4cbfec1c27cb1ad7034710c4822b8ed36036
Warren Buckley <wbuckley15@hotmail.com>
2026-02-17 12:12:17 -0500

    WIP - unit tests

---
+++ Compiler.Analysis.Test/Data/ExpressionFolder/ExpressionFolderData.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler.Analysis.Test.Data.ExpressionFolder;
+
+public class ExpressionFolderData
+{
+}
---
+++ Compiler.Analysis.Test/Tests/ExpressionFolderTests.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler.Analysis.Test.Tests;
+
+internal class ExpressionFolderTests
+{
+}
