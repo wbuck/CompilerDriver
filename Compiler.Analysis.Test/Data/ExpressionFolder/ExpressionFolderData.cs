fefa28ea510564e3f6a3275bb2baece63ed715d0
Warren Buckley <wbuckley15@hotmail.com>
2026-02-18 07:17:22 -0500

    WIP - unit tests

--- Compiler.Analysis.Test/Data/ExpressionFolder/ExpressionFolderData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Compiler.Analysis.Test.Data.ExpressionFolder;
-
-public class ExpressionFolderData
-{
-}
--- Compiler.Analysis.Test/Tests/ExpressionFolderTests.cs
+++ Compiler.Analysis.Test/Tests/ExpressionFolderTests.cs
@@ -1,9 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Runtime.InteropServices;
+using Compiler.Analysis.Helpers;
+using Compiler.Analysis.Validators;
+using Compiler.Lexer;
+using Compiler.Lexer.Tokens;
+using Compiler.Parser.Nodes;
 
 namespace Compiler.Analysis.Test.Tests;
 
-internal class ExpressionFolderTests
+public class ExpressionFolderTests
 {
+    [Theory]
+    [InlineData("1 + 2", 3)]
+    [InlineData("6 % 5 - 1", 0)]
+    [InlineData("4 / 2 * 10", 20)]
+    [InlineData("4 / 2 + 10 * 7", 72)]
+    [InlineData("24 / (2 + 10) * 7", 14)]
+    [InlineData("10 << 1", 20)]
+    [InlineData("10 >> 1", 5)]
+    [InlineData("5 & 1", 1)]
+    [InlineData("5 | 2", 7)]
+    [InlineData("5 ^ 1", 4)]
+    public void ShouldFoldExpression(string expression, int result)
+    {
+        var node = Parse
+        (
+            $$"""
+            int main(void) {
+                int x = {{expression}};
+            }                                  
+            """
+        );        
+        Assert.Equivalent
+        (
+            new ConstantNode<int>(result), 
+            ExpressionFolder.FoldExpression(node.Initializer!), 
+            true
+        );
+    }
+    
+    private static VariableDeclarationNode Parse(string fileContent)
+    {
+        var tokens = CollectionsMarshal.AsSpan(GetTokens(fileContent));
+        var validator = new SemanticValidator();
+        var node = validator.Validate(ProgramNode.Parse(ref tokens, fileContent.AsMemory()));
+        return (VariableDeclarationNode)((FunctionDeclarationNode)node.Nodes[0]).Body!.Items[0];
+    }
+    
+    private static List<IToken> GetTokens(ReadOnlySpan<char> fileContent)
+    {
+        Assert.True(LexicalAnalyzer.TryTokenize(fileContent, out var tokens));
+        return tokens;
+    }
 }
