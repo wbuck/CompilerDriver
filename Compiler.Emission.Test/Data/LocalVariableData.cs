ebb84033dcf92da5a11a8990f9b29b417cb43d7f
Warren Buckley <wbuckley15@hotmail.com>
2026-02-21 05:41:54 -0500

    WIP - unit tests

--- Compiler.Emission.Test/Data/LocalVariableData.cs
+++ Compiler.Emission.Test/Data/LocalVariableData.cs
@@ -164,24 +164,21 @@ public class LocalVariableData : DataBase
             }
             """,
             GetExpected([
-                "subq $32, %rsp",
-                "movl $2593, -4(%rbp)",
-                "negl -4(%rbp)",
-                "movl -4(%rbp), %r10d",
-                "movl %r10d, -8(%rbp)",
-                "movl -8(%rbp), %eax",
+                "subq $16, %rsp",
+                "movl $-2593, -4(%rbp)",
+                "movl -4(%rbp), %eax",
                 "cdq",
                 "movl $3, %r10d",
                 "idivl %r10d",
-                "movl %edx, -12(%rbp)",
-                "movl -12(%rbp), %r10d",
-                "movl %r10d, -8(%rbp)",
+                "movl %edx, -8(%rbp)",
                 "movl -8(%rbp), %r10d",
+                "movl %r10d, -4(%rbp)",
+                "movl -4(%rbp), %r10d",
+                "movl %r10d, -12(%rbp)",
+                "negl -12(%rbp)",
+                "movl -12(%rbp), %r10d",
                 "movl %r10d, -16(%rbp)",
-                "negl -16(%rbp)",
-                "movl -16(%rbp), %r10d",
-                "movl %r10d, -20(%rbp)",
-                "movl -20(%rbp), %eax"
+                "movl -16(%rbp), %eax"
             ])
         );
         Add
